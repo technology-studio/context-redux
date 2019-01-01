c1ba683c499942c451576ffe74209345749987cb
Rostislav Simonik <rostislav.simonik@technologystudio.sk>
2019-01-01 16:07:55 +0100

    Prevent creation of default context with initial state when not related action is processed.

--- __tests__/ContextReduxTest.js
+++ __tests__/ContextReduxTest.js
@@ -13,12 +13,20 @@ import { sampleArrayContextRedux } from '../sample'
 
 const SAMPLE_ITEM = 'sample-item'
 
-test('shoud pass', () => {
-  const state = sampleArrayContextRedux.reducer({}, sampleArrayContextRedux.creators.add({ item: SAMPLE_ITEM }))
+describe('array context redux', () => {
+  test('shoud add item to default context', () => {
+    const state = sampleArrayContextRedux.reducer({}, sampleArrayContextRedux.creators.add({ item: SAMPLE_ITEM }))
 
-  expect(state).toStrictEqual({
-    [DEFAULT_CONTEXT]: [
-      SAMPLE_ITEM,
-    ],
+    expect(state).toStrictEqual({
+      [DEFAULT_CONTEXT]: [
+        SAMPLE_ITEM,
+      ],
+    })
+  })
+
+  test('shoud not create default context initial state in case operation is not reducer related', () => {
+    const state = sampleArrayContextRedux.reducer({}, { type: 'VOID_ACTION' })
+
+    expect(state).toStrictEqual({})
   })
 })
